4ee73baebc4ae42d41b1d45821a5dd0caacfb3e6
MAbdullah005 <abdullahaliofc@gmail.com>
2025-07-23 18:03:08 -0700

    predicaiton outtput store done and alos test appliaction along with valid and output

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pymongo
 from NetworkSecurity.logging.logger import logging
 from NetworkSecurity.expection.expection import NetworkSecurityExpection
 from NetworkSecurity.pipeline.traning_pipeline import Traning_pipeline
+from NetworkSecurity.utils.ml_utils.model.estimator import NetworkModel
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI,File,UploadFile,Request
@@ -31,6 +32,9 @@ from NetworkSecurity.constant.traning_pipeline import DATA_INGESION_COLLECTION_N
 database=client[DATA_INGESION_DATABASE_NAME]
 collection=database[DATA_INGESION_COLLECTION_NAME]
  
+from fastapi.templating import Jinja2Templates
+template=Jinja2Templates(directory='./template')
+
 
 app=FastAPI()
 origins=['*']
@@ -56,6 +60,30 @@ async def train_route():
         return Response("Traning is sucessfull")
     except Exception as e:
         NetworkSecurityExpection(e,sys)
+
+    
+@app.post('/predict')
+async def predict_route(request:Request,file:UploadFile=File(...)):
+   try: 
+    df=pd.read_csv(file.file)
+    #print(df)
+    preprocessor=load_object('final_model/preprocessor.pkl')
+    model=load_object('final_model/model.pkl')
+    network_model=NetworkModel(preprocessor=preprocessor,model=model)
+    y_pred=network_model.predict(df)
+    #print(df)
+    df["predicted_column"]=y_pred
+    df["predicted_column"].replace(-1,0)
+    
+    df.to_csv("predication_output/output.csv")
+    table_html=df.to_html(classes='table table-striped')
+    #print(table_html)
+    return template.TemplateResponse("table.html",{"request":request,"table":table_html})
+   
+   except Exception as e:
+      raise NetworkSecurityExpection(e,sys)
+
+
 
 
 if __name__=='__main__':
